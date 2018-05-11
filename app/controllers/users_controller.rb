d00a799df51912da795bfb7f623d6a72332d11d9
Aaron Wagener <aaronjwagener@gmail.com>
2018-05-10 19:46:58 -0500

    add successful login test, login users when account created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,32 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
   def index
     @users = User.all
   end
 
-  # GET /users/1
   def show
     @user = User.find(params[:id])
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Plaza Calendar!"
+      log_in(@user)
       redirect_to @user
     else
       render 'new'
     end
   end
 
-  # PATCH/PUT /users/1
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -44,8 +39,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
